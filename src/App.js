01b5c04d7dc09ca4c1b0087d29e3cbf72b2c36f3
h1rish <39730312+h1rish@users.noreply.github.com>
2026-02-10 14:12:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div>
-      <h1>Hello SAP BTP HTML5!</h1>
-      <p>Your React app is ready to deploy.</p>
+      <h1>React App Deployed on SAP BTP</h1>
+      <p>API URL: {process.env.REACT_APP_API_URL}</p>
+      <p>Environment: {process.env.REACT_APP_ENV}</p>
     </div>
   );
 }
