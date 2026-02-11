ef5713573637254d69b9a4aabe61c0620764db30
h1rish <39730312+h1rish@users.noreply.github.com>
2026-02-11 09:44:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
+// Mount the App component into the root div in index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
